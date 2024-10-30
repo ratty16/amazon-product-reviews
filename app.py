e370eaea6552ab29833a882a1dcd7db752add6e7
Ratna Meena Shivakumar <114369971+ratty16@users.noreply.github.com>
2024-10-29 22:31:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
+import os
 import nltk
 from nltk.corpus import stopwords
-import os
 
 nltk.download('stopwords')
 
@@ -27,18 +25,6 @@ def clean_review(review):
     return " ".join(word for word in review.split() if word not in stp_words)
 
 data['Review'] = data['Review'].apply(clean_review)
-
-# Generate word clouds
-def generate_wordcloud(sentiment, filename):
-    consolidated = ' '.join(word for word in data['Review'][data['Sentiment'] == sentiment].astype(str))
-    wordcloud = WordCloud(width=1600, height=800, random_state=21, max_font_size=110).generate(consolidated)
-    plt.figure(figsize=(10, 6))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    wordcloud.to_file(os.path.join('static', filename))
-
-generate_wordcloud(0, 'wordcloud_neg.png')
-generate_wordcloud(1, 'wordcloud_pos.png')
 
 # Text Vectorization and Model Training
 cv = TfidfVectorizer(max_features=2500)
